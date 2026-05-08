caee676800d88e44b839bccaf0015b871deac67a
Hansika <hansikasaini97@gmail.com>
2026-05-08 16:14:48 +0530

    basic hero section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Navbar from "../components/home/navbar";
-//import Hero from "../components/home/hero";
+import Hero from "../components/home/hero";
 
 export default function Home() {
   return (
     <main className="bg-black text-white">
       <Navbar />
+      <Hero />
     </main>
   );
 }

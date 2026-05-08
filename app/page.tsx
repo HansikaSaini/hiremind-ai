78e708a49a3c04da222e8e9a1198ecc998745bf1
Hansika <hansikasaini97@gmail.com>
2026-05-08 13:46:39 +0530

    Initial project setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-6xl font-bold tracking-tight">
+        HireMind AI 🚀
+      </h1>
+    </main>
+  );
+}
